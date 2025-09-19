932371ebafb76d52a7149242f6a805fefef0a0b4
Максим <Max@Marias-MacBook-Air.local>
2025-09-19 13:53:35 +0300

    Removed the logic for processing updates from the main

---
+++ handler/handler.go
@@ -0,0 +1,64 @@
+package handler
+
+import (
+	"fmt"
+	"log"
+	"math"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/mewerm/weatherbot/clients/openweather"
+)
+
+type Handler struct {
+	bot      *tgbotapi.BotAPI
+	owClient *openweather.OpenWeatherClient
+}
+
+func New(bot *tgbotapi.BotAPI, owCleint *openweather.OpenWeatherClient) *Handler {
+	return &Handler{
+		bot:      bot,
+		owClient: owCleint,
+	}
+}
+
+func (h *Handler) Start() {
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := h.bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		h.HandlerUpdate(update)
+	}
+}
+
+func (h *Handler) HandlerUpdate(update tgbotapi.Update) {
+	if update.Message != nil { // If we got a message
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		coordinates, err := h.owClient.Coordinats(update.Message.Text)
+		if err != nil {
+
+			msg := tgbotapi.NewMessage(
+				update.Message.Chat.ID,
+				"Не смогли получить координаты")
+			msg.ReplyToMessageID = update.Message.MessageID
+			h.bot.Send(msg)
+			return
+		}
+
+		weather, err := h.owClient.Weather(coordinates.Lat, coordinates.Lon)
+		if err != nil {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Не смогли получить погоду в этой местности")
+			msg.ReplyToMessageID = update.Message.MessageID
+			h.bot.Send(msg)
+			return
+		}
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID,
+			fmt.Sprintf("Температура в %s: %d°C", update.Message.Text, int(math.Round(weather.Temp)))) //Round
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		h.bot.Send(msg)
+	}
+}
--- main.go
+++ main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"math"
 	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
 	"github.com/mewerm/weatherbot/clients/openweather"
+	"github.com/mewerm/weatherbot/handler"
 )
 
 func main() {
@@ -25,40 +24,10 @@ func main() {
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	updates := bot.GetUpdatesChan(u)
-
 	owClient := openweather.New(os.Getenv("OPENWEATHERAPI_KEY"))
 
-	for update := range updates {
-		if update.Message != nil { // If we got a message
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-			coordinates, err := owClient.Coordinats(update.Message.Text)
-			if err != nil {
+	botHandler := handler.New(bot, owClient)
 
-				msg := tgbotapi.NewMessage(
-					update.Message.Chat.ID,
-					"Не смогли получить координаты")
-				msg.ReplyToMessageID = update.Message.MessageID
-				bot.Send(msg)
-				continue
-			}
+	botHandler.Start()
 
-			weather, err := owClient.Weather(coordinates.Lat, coordinates.Lon)
-			if err != nil {
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Не смогли получить погоду в этой местности")
-				msg.ReplyToMessageID = update.Message.MessageID
-				bot.Send(msg)
-				continue
-			}
-
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID,
-				fmt.Sprintf("Температура в %s: %d°C", update.Message.Text, int(math.Round(weather.Temp)))) //Round
-			msg.ReplyToMessageID = update.Message.MessageID
-
-			bot.Send(msg)
-		}
-	}
 }
